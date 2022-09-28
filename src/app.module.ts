81937cb437773ae026e3e65142e58791e7c3489f
Kirill Zagul <zagulkirill@gmail.com>
2022-09-28 17:44:21 +0500

    Обновлен tsconfig.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,6 @@ import {
   ClientController
  } from './controllers/index';
 
-// import { MeetingController } from './controllers/meeting/meeting.controller';
-// import { ClientController } from './controllers/client/client.controller';
-
 @Module({
   imports: [],
   controllers: [AppController, MeetingController, ClientController],
