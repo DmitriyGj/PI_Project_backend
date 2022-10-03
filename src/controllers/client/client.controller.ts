b52fef30f4aac4916692ba84cc28c41d1d2ee02b
Kirill Zagul <zagulkirill@gmail.com>
2022-10-03 15:34:59 +0500

    Добавлено Delete для Client

--- src/controllers/client/client.controller.ts
+++ src/controllers/client/client.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ClientService } from '../.././providers/client.service';
 import { Client } from '../../models/client/client.interface';
 import { from, Observable } from "rxjs";
-import { UpdateResult } from 'typeorm';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 @Controller('client')
 export class ClientController {
@@ -28,5 +28,10 @@ export class ClientController {
   @Put(':id')
   update(@Param('id') id: number, @Body() сlient: Client): Observable<UpdateResult> {
     return this.clientService.update(id, сlient);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Observable<DeleteResult> {
+    return this.clientService.delete(id);
   }
 }
--- src/providers/client.service.ts
+++ src/providers/client.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ClientEntity } from "../models/client/client.entity";
 import { Client } from "../models/client/client.interface";
-import { Repository, UpdateResult } from "typeorm";
+import { DeleteResult, Repository, UpdateResult } from "typeorm";
 import { from, Observable } from "rxjs";
 
 @Injectable()
@@ -25,5 +25,9 @@ export class ClientService {
 
     update(id: number, client: Client): Observable<UpdateResult> {
         return from(this.clientRepository.update(id, client));
+    }
+
+    delete(id: number): Observable<DeleteResult> {
+        return from(this.clientRepository.delete({id}))
     }
 }
