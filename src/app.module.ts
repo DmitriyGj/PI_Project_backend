4bb3e73529dae3289fba0c6217d090f86887fd08
Kirill Zagul <zagulkirill@gmail.com>
2022-11-06 02:03:02 +0500

    add config module and dependencies for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,9 @@ import { AppService } from './app.service';
 import { ClientModule } from './modules/client.module';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { Config, DataBaseModule } from './config/index'
+
+// import { ConfigModule } from '@nestjs/config';
 
 import { 
   MeetingController,
@@ -14,26 +16,29 @@ import {
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true
+    Config,
+    // ConfigModule.forRoot({ isGlobal: true }),
+
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.POSTGRES_HOST,
+    //   port: parseInt(<string>process.env.POSTGRES_PORT),
+    //   username: process.env.POSTGRES_USER,
+    //   password: process.env.POSTGRES_PASSWORD,
+    //   database: process.env.POSTGRES_DATABASE,
+    //   autoLoadEntities: true,
+    //   synchronize: true
       
-      // type: 'postgres',
-      // host: process.env.HEROKU_URL,
-      // port: parseInt(<string>process.env.HEROKU_PORT),
-      // username: process.env.HEROKU_USER,
-      // password: process.env.HEROKU_PASSWORD,
-      // database: process.env.HEROKU_DATABASE,
-      // autoLoadEntities: true,
-      // synchronize: true
-    }),
+    //   // type: 'postgres',
+    //   // host: process.env.HEROKU_URL,
+    //   // port: parseInt(<string>process.env.HEROKU_PORT),
+    //   // username: process.env.HEROKU_USER,
+    //   // password: process.env.HEROKU_PASSWORD,
+    //   // database: process.env.HEROKU_DATABASE,
+    //   // autoLoadEntities: true,
+    //   // synchronize: true
+    // }),
+    DataBaseModule,
     ClientModule
   ],
   controllers: [
